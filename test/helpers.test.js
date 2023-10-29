8301f1b188beb82396b6d2ff2a0abc0551c36797
dzdidi <deniszalessky@gmail.com>
2023-10-29 11:33:37 +0100

    helper: parseRemote
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -1,5 +1,5 @@
 const test = require('brittle')
-const { parseLine } = require('../lib/helpers.js')
+const { parseLine, parseRemoteLine } = require('../lib/helpers.js')
 
 test('parseLine', (t) => {
   let line = '1,2'
@@ -17,3 +17,18 @@ test('parseLine', (t) => {
   t.exception(() => parseLine(line), 'invalid input: must be 2d array of integers in range')
 })
 
+test('parseRemoteLine', (t) => {
+  let line = '[1,2]'
+  let res = parseRemoteLine(line)
+  t.is(res[0], 1)
+  t.is(res[1], 2)
+
+  line = '[1,2,3]'
+  t.exception(() => parseRemoteLine(line), 'invalid input: must be 2d array')
+
+  line = '[1,"a"]'
+  t.exception(() => parseRemoteLine(line), 'invalid input: must be 2d array of integers')
+
+  line = '[1,11]'
+  t.exception(() => parseRemoteLine(line), 'invalid input: must be 2d array of integers in range')
+})
