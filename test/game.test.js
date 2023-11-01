848dd9cc1e768eb83ebc131981862337e5d0682f
dzdidi <deniszalessky@gmail.com>
2023-11-01 17:08:41 +0100

    game: test coverage
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- test/game.test.js
+++ test/game.test.js
@@ -1,4 +1,5 @@
 const test = require('brittle')
+
 const createTestnet = require('@hyperswarm/testnet')
 
 const { Game } = require('../lib/game.js')
@@ -25,13 +26,12 @@ test('Game:init', async (t) => {
   const { bootstrap } = await createTestnet(3, t.teardown)
   const game = new Game(bootstrap)
 
-  await game.init()
+  await game.init(t.pass)
 
   t.ok(game.me.key)
 
   await game.exitCallback()
 })
-
 
 test('addMove', async (t) => {
   const game = new Game()
@@ -75,16 +75,82 @@ test('announceWinner', async (t) => {
   await game.exitCallback()
 })
 
-//test('Game:e2e', async (t) => {
-//  const testnet = await createTestnet(3, t.teardown)
-//  const game = new Game(testnet.bootstrap)
-//
-//  await game.init()
-//  //const them = testnet.createNode()
-//  //await game.start(them.defaultKeyPair.publicKey.toString('hex'))
-//  await game.start(testnet.nodes[0].defaultKeyPair.publicKey.toString('hex'))
-//
-//  t.ok(game.them.key)
-//
-//  await game.exitCallback()
-//})
+test('Game:e2e', async (t) => {
+  const testnet = await createTestnet(3, t.teardown)
+  const gameA = new Game(testnet.bootstrap)
+  const gameB = new Game(testnet.bootstrap)
+
+  await gameA.init(t.pass)
+  await gameB.init(t.pass)
+
+  await gameA.start(gameB.me.key.toString('hex'))
+  await gameB.start(gameA.me.key.toString('hex'))
+
+  t.is(gameA.them.key.toString('hex'), gameB.me.key.toString('hex'))
+  t.is(gameB.them.key.toString('hex'), gameA.me.key.toString('hex'))
+
+  await gameA.handleMyInput('0,0')
+  await new Promise((resolve, _reject) => setTimeout(resolve, 500))
+
+  t.is(gameA.myHull.length, 1)
+  t.alike(gameA.myHull, [[0, 0]])
+  t.is(gameA.theirHull.length, 0)
+  t.alike(gameA.theirHull, [])
+
+  t.is(gameB.myHull.length, 0)
+  t.alike(gameB.myHull, [])
+  t.is(gameB.theirHull.length, 1)
+  t.alike(gameB.theirHull, [[0, 0]])
+
+  await gameB.handleMyInput('1,1')
+  await new Promise((resolve, _reject) => setTimeout(resolve, 500))
+
+  t.is(gameA.myHull.length, 1)
+  t.alike(gameA.myHull, [[0, 0]])
+  t.is(gameA.theirHull.length, 1)
+  t.alike(gameA.theirHull, [[1, 1]])
+
+  t.is(gameB.myHull.length, 1)
+  t.alike(gameB.myHull, [[1, 1]])
+  t.is(gameB.theirHull.length, 1)
+  t.alike(gameB.theirHull, [[0, 0]])
+
+  await gameA.handleMyInput('1,0')
+  await new Promise((resolve, _reject) => setTimeout(resolve, 500))
+
+  t.is(gameA.myHull.length, 2)
+  t.alike(gameA.myHull, [[0, 0], [1, 0]])
+  t.is(gameA.theirHull.length, 1)
+  t.alike(gameA.theirHull, [[1, 1]])
+
+  t.is(gameB.myHull.length, 1)
+  t.alike(gameB.myHull, [[1, 1]])
+  t.is(gameB.theirHull.length, 2)
+  t.alike(gameB.theirHull, [[0, 0], [1, 0]])
+
+  await gameB.handleMyInput('0,1')
+  await new Promise((resolve, _reject) => setTimeout(resolve, 500))
+
+  t.is(gameA.myHull.length, 2)
+  t.alike(gameA.myHull, [[0, 0], [1, 0]])
+  t.is(gameA.theirHull.length, 2)
+  t.alike(gameA.theirHull, [[0, 1], [1, 1]])
+
+  t.is(gameB.myHull.length, 2)
+  t.alike(gameB.myHull, [[0, 1], [1, 1]])
+  t.is(gameB.theirHull.length, 2)
+  t.alike(gameB.theirHull, [[0, 0], [1, 0]])
+
+  await gameA.handleMyInput('0,1')
+  await new Promise((resolve, _reject) => setTimeout(resolve, 500))
+
+  t.is(gameA.myHull.length, 3)
+  t.alike(gameA.myHull, [[0, 0], [1, 0], [0, 1]])
+  t.is(gameA.theirHull.length, 2)
+  t.alike(gameA.theirHull, [[0, 1], [1, 1]])
+
+  t.is(gameB.myHull.length, 2)
+  t.alike(gameB.myHull, [[0, 1], [1, 1]])
+  t.is(gameB.theirHull.length, 3)
+  t.alike(gameB.theirHull, [[0, 0], [1, 0], [0, 1]])
+})
